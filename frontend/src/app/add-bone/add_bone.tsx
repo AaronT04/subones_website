3d3e57d7b00765e735867627188886737fe36d11
SamCode-BD <sam051704@gmail.com>
2025-12-04 14:53:05 -0500

    pageManager

--- frontend/src/app/add-bone/add_bone.tsx
+++ frontend/src/app/add-bone/add_bone.tsx
@@ -23,7 +23,6 @@ import {
    
   export function BoneMenu() {
 
-    let [boneName, setBoneName] = useState("bone name");
     const router = useRouter();
     PageManager.connectRouter(router);
 
@@ -41,8 +40,7 @@ import {
                 <div>
                   {metrics_list.cranial_metrics.map((name, i) => <Button className="text-left" variant="ghost" name={name} key={i} 
                     onClick={() => {
-                      setBoneName(name);
-                      router.push(`/bone-editor?boneName=${encodeURIComponent(name)}`);
+                       PageManager.handleCreateBone(name)
                     }}
                     >
                     {name} </Button>)}
@@ -53,7 +51,8 @@ import {
               <div className="flex flex-wrap justify-evenly max-h-150 gap-2 w-full">
                 {metrics_list.postcranial_metrics.map((name, i) => 
                 <Button2 className="text-left text-white bg-maroon hover:bg-maroon2" variant="default" name={name} key={i} 
-                onClick={() => PageManager.handleCreateBone(name)}
+                  onClick={() => 
+                    PageManager.handleCreateBone(name)}
                 > 
                 {name} </Button2>)}
               </div>
